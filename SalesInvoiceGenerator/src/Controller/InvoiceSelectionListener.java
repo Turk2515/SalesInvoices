dd6e98f0d46f73efa2d176c79d08407c8f726f39
Mostafa Maher <54922619+Turk2515@users.noreply.github.com>
2022-05-19 20:57:33 +0200

    Add files via upload

---
+++ SalesInvoiceGenerator/src/Controller/InvoiceSelectionListener.java
@@ -0,0 +1,39 @@
+package Controller;
+
+import static Controller.ActionsListener.simpleDateFormat;
+import Models.Header;
+import Models.Items;
+import Models.ItemsTable;
+import View.AppMainFrame;
+import java.util.ArrayList;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class InvoiceSelectionListener implements ListSelectionListener{
+
+    private AppMainFrame appFrame;
+
+    public InvoiceSelectionListener(AppMainFrame appFrame) {
+        this.appFrame = appFrame;
+    }
+
+    public InvoiceSelectionListener() {
+    }
+    
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        int selectedInoviceIndex = appFrame.getInvoicesTable().getSelectedRow();
+        if (selectedInoviceIndex != -1){
+            Header selectedInvoice = appFrame.getHeaderArrayList().get(selectedInoviceIndex);
+            ArrayList<Items> itemArrayList = selectedInvoice.getLines();
+            ItemsTable itemsTable = new ItemsTable(itemArrayList);
+            appFrame.setItemsArrayList(itemArrayList);
+            appFrame.getInvoiceItemsTable().setModel(itemsTable);
+            appFrame.getInvoiceNumerValueLabel().setText("" + selectedInvoice.getInvoiceNumber());
+            appFrame.getCustomerNameValueLabel().setText(selectedInvoice.getCutomer());
+            appFrame.getInvoiceDateValueLabel().setText(simpleDateFormat.format(selectedInvoice.getInvoiceDate()));
+            appFrame.getInvoiceTotalValueLabel().setText("" + selectedInvoice.getTotal());
+        }      
+    }
+    
+}
